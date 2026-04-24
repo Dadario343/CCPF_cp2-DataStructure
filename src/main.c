2489d592d42e296d410a0b37d41abce87f3a24bf
pedrosoaresdesouza03@gmail.com <labsfiap@fiap.com.br>
2026-04-24 11:09:02 -0300

    opcao para usuario escolher

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main() {
     printf("1. Digitar notas\n");
     printf("2. Digitar frequencia do aluno\n");
     printf("3. Sair do sistema\n");
-
+    printf("Opcao: ");
     
     return 0;
 }
