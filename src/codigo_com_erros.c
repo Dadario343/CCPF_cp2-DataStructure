7a7f8834a031fea976510ac963f787a0b4927212
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-24 10:48:15 -0300

    colocando no if o maior para que seja otimizado

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    float nota1, nota2, media;
+    float nota1;
+    float nota2;
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
@@ -9,9 +10,9 @@ int main() {
     printf("Digite a segunda nota: ");
     scanf("%f", nota2);
 
-    media = nota1 + nota2 / 2;
+    float media = nota1 + nota2 / 2.0;
 
-    if (media = 6) {
+    if (media >= 6) {
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
