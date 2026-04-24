73e4c551deefec9398598f9d0c655f0e095f6244
rengelnickolas-ctrl <rengelnickolas@gmail.com>
2026-04-24 11:25:10 -0300

    Adicionado a opcao de digitar a segunda nota

--- src/main.c
+++ src/main.c
@@ -25,6 +25,10 @@ int main() {
 
         printf("Digite a primeira nota: ");
         scanf("%f", &nota1);
+
+        printf("Digite a segunda nota: ");
+        scanf("%f", nota2);
+
     }
 
     return 0;
