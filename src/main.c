6f11351f5e1666a4e4290c16632aecf922d00972
mateusofneves <prmateus.ofn@gmail.com>
2026-04-24 11:30:43 -0300

    adicionando a terceira nota

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ int main() {
         
         printf("Opção Invalida\n");
         printf("Digite Novamente\n");
-    
+        menu();
     }
     
     switch (opcao) {
@@ -27,12 +27,16 @@ int main() {
         case 1:
             float nota1;
             float nota2;
+            float nota3;
 
-        printf("Digite a primeira nota: ");
-        scanf("%f", &nota1);
+            printf("Digite a primeira nota: ");
+            scanf(" %f", &nota1);
 
-        printf("Digite a segunda nota: ");
-        scanf("%f", nota2);
+            printf("Digite a segunda nota: ");
+            scanf(" %f", &nota2);
+
+            printf("Digite a terceira nota: ");
+            scanf(" %f", &nota3);
 
     }
 
