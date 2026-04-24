91ccc7ca1b199dbcaa89c6f4bdd2c3224348e516
pedrosoaresdesouza03@gmail.com <labsfiap@fiap.com.br>
2026-04-24 11:06:02 -0300

    Opção 3 sair do sistema

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@ int main() {
     printf("\n===== Bem vindo ao sistema de notas =====\n");
     printf("1. Digitar notas\n");
     printf("2. Digitar frequencia do aluno\n");
+    printf("3. Sair do sistema\n");
+
     
     return 0;
 }
