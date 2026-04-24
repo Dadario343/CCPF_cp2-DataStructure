9fa31dacdeb2b75f03b6dde3842dffa65c882ff8
Dadario343 <olavodadariovb@gmail.com>
2026-04-24 11:03:48 -0300

    Adicionado segunda opção

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@ int main() {
 
     printf("\n===== Bem vindo ao sistema de notas =====\n");
     printf("1. Digitar notas\n");
+    printf("2. Digitar frequencia do aluno\n");
     
     return 0;
 }
